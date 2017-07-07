4d1f485f9aa0d60544112246512671908add7748
NHClement <clement.delouvencourt@gmail.com>
2017-07-07 12:36:18 +0200

    Mise en place du projet

---
+++ server/main.js
@@ -0,0 +1,13 @@
+import { Meteor } from 'meteor/meteor';
+
+Meteor.startup(() => {
+    ServiceConfiguration.configurations.remove({
+        service: "facebook"
+    });
+
+    ServiceConfiguration.configurations.insert({
+        service: "facebook",
+        appId: '1912649022307785',
+        secret: 'afe68df1a55ebc24c479234b1104c757'
+    });
+});
