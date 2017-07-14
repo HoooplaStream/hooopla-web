cef41aa43199743e218f1afe9f83285cfedf2fb0
PHClement <clement.delouvencourt@gmail.com>
2017-07-14 23:26:02 +0200

    Add rating

--- client/lib/template.js
+++ client/lib/template.js
@@ -1,8 +1,26 @@
 Template.registerHelper('math', function () {
     return {
-        mul ( a, b ) { return a * b; },
-        div ( a, b ) { return b ? a / b : 0; },
-        sum ( a, b ) { return a + b; },
-        sub ( a, b ) { return a - b; },
+        mul (a, b) {
+            return a * b;
+        },
+        div (a, b) {
+            return b ? a / b : 0;
+        },
+        sum (a, b) {
+            return a + b;
+        },
+        sub (a, b) {
+            return a - b;
+        },
     }
+});
+
+Template.registerHelper('starNote', function (note) {
+    var noteFive = note / 2;
+    var out = '';
+    for (var i = 1; i < noteFive; i++)
+        out += '<i class="material-icons white-text" style="font-size: 18px">star</i>';
+    for (var i2 = noteFive; i2 < 5; i2++)
+        out += '<i class="material-icons grey-text" style="font-size: 18px">star</i>';
+    return Spacebars.SafeString(out);
 });
